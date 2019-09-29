05d173b6c5b60a023957d52816beed4d88f84afa
VikinGSpb <imbot@inbox.ru>
2019-09-29 13:10:00 +0300

    feat: add first version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    let firstMas = first.split('');
+    let secondMas = second.split('');
+    let multiplyMas = [];
+
+    for(let i = firstMas.length - 1; i >= 0; i--)
+        for(let j = secondMas.length - 1; j >= 0; j--)
+        {
+            let number = firstMas[i] * secondMas[j];
+            multiplyMas[i + j] ? multiplyMas[i + j] += number : multiplyMas[i + j] = number;
+            if(multiplyMas[i + j] >= 10)
+            {
+                multiplyMas[i + j - 1] += Math.floor(multiplyMas[i + j] / 10);
+                multiplyMas[i + j] %= 10;
+            }
+        }
+    return multiplyMas.join('');
 }
