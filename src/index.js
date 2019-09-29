c8ac788187257068f153fd5bb7d891e7c1852123
VikinGSpb <imbot@inbox.ru>
2019-09-29 13:58:07 +0300

    fix: reverse arrays for good first number

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 module.exports = function multiply(first, second) {
-    let firstMas = first.split('');
-    let secondMas = second.split('');
+    let firstMas = first.split('').reverse();
+    let secondMas = second.split('').reverse();
     let multiplyMas = [];
 
-    for(let i = firstMas.length - 1; i >= 0; i--)
-        for(let j = secondMas.length - 1; j >= 0; j--)
+    for(let i = 0; i < firstMas.length; i++)
+        for(let j = 0; j < secondMas.length; j++)
         {
             let number = firstMas[i] * secondMas[j];
             multiplyMas[i + j] ? multiplyMas[i + j] += number : multiplyMas[i + j] = number;
             if(multiplyMas[i + j] >= 10)
             {
-                multiplyMas[i + j - 1] ? multiplyMas[i + j - 1] += Math.floor(multiplyMas[i + j] / 10) : multiplyMas[i + j - 1] = Math.floor(multiplyMas[i + j] / 10);
+                multiplyMas[i + j + 1] ? multiplyMas[i + j + 1] += Math.floor(multiplyMas[i + j] / 10) : multiplyMas[i + j + 1] = Math.floor(multiplyMas[i + j] / 10);
                 multiplyMas[i + j] %= 10;
             }
         }
-    return multiplyMas.join('');
+    return multiplyMas.reverse().join('');
 }
